649d1b15abe4aab951e023e3217baa6aa0a26fa1
Hal <waikei1917oct@gmail.com>
2017-03-11 11:42:45 -0700

    Haha Push

--- src/com/HelloWorld/Main.java
+++ src/com/HelloWorld/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code herep
         System.out.println("Hello World!!!");
+        System.out.println("Haha Hello World!!!");
 
     }
 }
